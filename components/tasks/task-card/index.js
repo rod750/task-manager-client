3b43ba68475c65d377dd7d26e7b897525796be2d
Rodrigo D. Cuervo <rod750@gmail.com>
2020-10-15 12:52:39 -0500

    Added timer ui and mark as complete button on task card

--- components/tasks/task-card/index.js
+++ components/tasks/task-card/index.js
@@ -1,19 +1,43 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, Nav } from "react-bootstrap"
+import { Timer } from "../../shared"
+import { RiPencilFill, RiDeleteBinFill, RiCheckFill } from "react-icons/ri"
 
 export function TaskCard({
   task,
   onEdit = () => {},
-  onDelete = () => {}
+  onDelete = () => {},
+  onSuccess = () => {}
 }) {
   return (
     <Card>
+      <Card.Header>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{ task.name }</Card.Title>
         <Card.Text>{ task.description }</Card.Text>
       </Card.Body>
       <Card.Body>
-        <Card.Link onClick={onEdit}>Editar</Card.Link>
-        <Card.Link onClick={onDelete}>Eliminar</Card.Link>
+        <Card.Link><Timer /></Card.Link>
+        <Card.Link>
+          <Button
+            onClick={onSuccess}
+            variant="success">
+            <RiCheckFill /> Completar
+          </Button>
+        </Card.Link>
+        <Card.Link>
+          <Button
+            onClick={onEdit}>
+            <RiPencilFill /> Editar
+          </Button>
+        </Card.Link>
+        <Card.Link>
+          <Button
+            onClick={onDelete}
+            variant="danger">
+            <RiDeleteBinFill /> Eliminar
+          </Button>
+        </Card.Link>
       </Card.Body>
     </Card>
   )
