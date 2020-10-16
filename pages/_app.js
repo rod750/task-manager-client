678e9cba3d477fc0fdfb96a8ae1bdb2a2a6e910d
Rodrigo D. Cuervo <rod750@gmail.com>
2020-10-16 00:21:55 -0500

    Loaded moment duration format plugin

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "../styles/index.sass"
 
+import moment from "moment"
+import momentDurationFormatSetup from "moment-duration-format"
+
+momentDurationFormatSetup(moment)
+
 export default function TaskManager({ Component, pageProps }) {
   return (
     <Component { ...pageProps } />
